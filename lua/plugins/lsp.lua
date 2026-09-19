a3390023f4f13f97d4e785a1f07af0f392c37e76
Richard Adiguna <richardadiguna@MacBookPro.lan>
2026-09-18 22:35:46 -0400

    Finalize basic python LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,11 +5,30 @@ return {
       servers = {
         basedpyright = {
           settings = {
-            python = {
+            basedpyright = {
               analysis = {
-                useLibraryCodeForTypes = true,
+                -- Don't enforce Python type annotations
+                typeCheckingMode = "off",
+
+                -- Only analyze files you currently have open
                 diagnosticMode = "openFilesOnly",
-                typeCheckingMode = "standard",
+
+                -- Better autocomplete/type information for libraries
+                useLibraryCodeForTypes = true,
+
+                -- Suggest imports automatically
+                autoImportCompletions = true,
+
+                -- Automatically recognize common paths like src/
+                autoSearchPaths = true,
+
+                -- Optional: reduce visual noise
+                inlayHints = {
+                  variableTypes = false,
+                  callArgumentNames = false,
+                  functionReturnTypes = false,
+                  genericTypes = false,
+                },
               },
             },
           },
